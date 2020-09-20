0b605642c3f5da1b1e3c8b9134e3163df8134223
Gapur Kassym <gapur.kassym@gmail.com>
2020-09-20 21:47:52 +0600

    feat(tsconfig): change tsconfig and eslint

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
