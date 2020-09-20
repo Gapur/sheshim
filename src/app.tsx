8681ca48f2d8f415b6a5344b153066545a8d4087
Gapur Kassym <gapur.kassym@gmail.com>
2020-09-20 22:48:13 +0600

    feat(src): styled-components

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  margin-left: 12px;
+`
 
 export default function App() {
-  return <div className="App">Sheshim</div>
+  return (
+    <div className="App">
+      <Title>Sheshim</Title>
+    </div>
+  )
 }
