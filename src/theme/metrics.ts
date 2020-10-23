a5cba8536f339ca04163544c6f78a7f7bdb14630
Gapur Kassym <gapur.kassym@gmail.com>
2020-10-23 23:06:05 +0600

    feat(theme): create theme

---
+++ src/theme/metrics.ts
@@ -0,0 +1,11 @@
+export default {
+  device_size: {
+    mobile_s: '320px',
+    mobile_m: '375px',
+    mobile_l: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptop_l: '1440px',
+    desktop: '2560px',
+  },
+}
