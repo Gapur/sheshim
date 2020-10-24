ccaf26da6c2f238665a8976f13182b72fb047fe6
Gapur Kassym <gapur.kassym@gmail.com>
2020-10-24 15:22:15 +0600

    feat(login): update auth layout

--- src/screens/auth/login/login.tsx
+++ src/screens/auth/login/login.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container, Grid, Button, Form, Icon, Segment, Image } from 'semantic-ui-react'
+import { Grid, Button, Form, Icon, Segment, Image } from 'semantic-ui-react'
 
 import { images } from '../../../assets'
 
-const Layout = styled.div`
-  background: #eff0f1;
+const AuthLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: #f6f7f9;
 `
 
 const LoginButton = styled(Button)`
@@ -15,32 +19,39 @@ const LoginButton = styled(Button)`
   }
 `
 
+const Logo = styled(Image)`
+  width: 72px;
+  margin-bottom: 32px;
+`
+
 export function Login() {
   return (
-    <Layout>
-      <Container>
-        <Grid columns={3} centered>
-          <Grid.Column>
-            <Image src={images.logo} size="tiny" centered />
-            <LoginButton basic fluid>
-              <Icon name='google' />Log in with Google
-            </LoginButton>
-            <LoginButton color='facebook' fluid>
-              <Icon name='facebook' />Log in with Facebook
-            </LoginButton>
-            <Segment>
-              <Form>
-                <Form.Field>
-                  <label>Email</label>
-                  <input placeholder='email' />
-                </Form.Field>
-                <Form.Input label='Password' type='password' placeholder="password" />
-                <Button type='submit' fluid color="twitter">Log in</Button>
-              </Form>
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </Layout>
+    <AuthLayout>
+      <Grid columns={3} centered container stackable>
+        <Grid.Column>
+          <Logo src={images.logo} centered />
+          <LoginButton basic fluid>
+            <Icon name="google" />
+            Log in with Google
+          </LoginButton>
+          <LoginButton color="facebook" fluid>
+            <Icon name="facebook" />
+            Log in with Facebook
+          </LoginButton>
+          <Segment>
+            <Form>
+              <Form.Field>
+                <label>Email</label>
+                <input placeholder="email" />
+              </Form.Field>
+              <Form.Input label="Password" type="password" placeholder="password" />
+              <Button type="submit" fluid color="twitter">
+                Log in
+              </Button>
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </AuthLayout>
   )
 }
--- src/theme/global-styles.ts
+++ src/theme/global-styles.ts
@@ -1,9 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
 
 const globalStyle = createGlobalStyle`
   body {
     font-family: 'Source Sans Pro', sans-serif;
+    margin: 0;
+    padding: 0;
   }
-`;
+`
 
-export default globalStyle;
+export default globalStyle
