d159f6e113bff3b9df2c5837dcf8a0d3b17a15fe
Gapur Kassym <gapur.kassym@gmail.com>
2020-10-29 22:22:12 +0600

    feat(users): create users table

--- src/screens/users/users.tsx
+++ src/screens/users/users.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Header, Table, Image } from 'semantic-ui-react'
 
 import { AppLayout } from '../../components'
+import { data } from './mock'
 
 export function Users() {
   return (
     <AppLayout page="users">
-      <span>Users</span>
+      <Header>Users</Header>
+      <Table basic="very" celled collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Username</Table.HeaderCell>
+            <Table.HeaderCell>Reputation</Table.HeaderCell>
+            <Table.HeaderCell>Position</Table.HeaderCell>
+            <Table.HeaderCell>Location</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.map((item) => (
+            <Table.Row key={item.id}>
+              <Table.Cell>
+                <Header as="h4" image>
+                  <Image src={item.avatar} rounded size="mini" />
+                  <Header.Content>
+                    {`${item.firstName} ${item.lastName}`}
+                    <Header.Subheader>{item.email}</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>{item.reputation}</Table.Cell>
+              <Table.Cell>{item.position}</Table.Cell>
+              <Table.Cell>{`${item.city}, ${item.country}`}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </AppLayout>
   )
 }
