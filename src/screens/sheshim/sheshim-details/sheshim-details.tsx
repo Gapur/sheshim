9ea507064d15344e4da03c3649e568eeba9aea4b
Gapur Kassym <gapur.kassym@gmail.com>
2020-11-04 23:56:53 +0600

    feat(sheshim-details): update details page

--- src/screens/sheshim/sheshim-details/sheshim-details.tsx
+++ src/screens/sheshim/sheshim-details/sheshim-details.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Label, Divider, Button } from 'semantic-ui-react'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { AppLayout } from '../../../components'
 import { CodeEditor } from './components/code-editor'
@@ -9,6 +10,14 @@ import { data } from '../../home/mock'
 interface SheshimDetailsParams {
   sheshimId: string
 }
+
+const Sheshimder = styled.div`
+  width: 72%;
+`
+
+const SheshimResponse = styled.div`
+  display: flex;
+`
 
 export function SheshimDetails() {
   const { sheshimId } = useParams<SheshimDetailsParams>()
@@ -21,9 +30,28 @@ export function SheshimDetails() {
 
   return (
     <AppLayout page="sheshim">
-      <Header>{sheshim.title}</Header>
-
-      <CodeEditor initialValue={sheshim.body} />
+      <Header>
+        {sheshim.title}
+        <Button color="twitter" floated="right">
+          Ask Question
+        </Button>
+      </Header>
+      <Label.Group tag>
+        <Label as="a">{`Asked: ${sheshim.createdAt}`}</Label>
+        <Label as="a">{`Responded: ${sheshim.answers}`}</Label>
+        <Label as="a">{`Viewed: ${sheshim.views} times`}</Label>
+      </Label.Group>
+      <Divider />
+      <Sheshimder>
+        <SheshimResponse>
+          <Button.Group size="mini" vertical>
+            <Button icon="angle up" />
+            <Button>{sheshim.votes}</Button>
+            <Button icon="angle down" />
+          </Button.Group>
+          <CodeEditor initialValue={sheshim.body} />
+        </SheshimResponse>
+      </Sheshimder>
     </AppLayout>
   )
 }
