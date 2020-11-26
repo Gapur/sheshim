5d627b5bd3afc29e3cbeb7a06027c9251d2a16ae
Gapur Kassym <gapur.kassym@gmail.com>
2020-11-27 00:43:16 +0600

    feat(auth): forgot passwor form

--- src/screens/auth/forgot-password/forgot-password.tsx
+++ src/screens/auth/forgot-password/forgot-password.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { Grid, Button, Form, Segment } from 'semantic-ui-react'
+import { Grid, Button, Form, Segment, Label } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 import { AuthLayout } from 'components'
+
+const REGEX_EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
 const Span = styled.span`
   display: block;
@@ -11,16 +14,41 @@ const Span = styled.span`
   padding-top: 16px;
 `
 
+interface FormValues {
+  email: string
+}
+
 export function ForgotPassword() {
+  const { errors, register, handleSubmit, setValue, trigger } = useForm<FormValues>()
+
+  useEffect(() => {
+    register('email', { required: 'Email is required', pattern: { value: REGEX_EMAIL, message: 'Email is not valid' } })
+  }, [register])
+
+  const onInputChange = async (name: 'email', value: string) => {
+    setValue(name, value)
+    await trigger(name)
+  }
+
+  const onSubmit = (data: FormValues) => console.log(data)
+
   return (
     <AuthLayout>
       <Grid columns={3} centered container stackable>
         <Grid.Column>
           <Segment raised>
-            <Form>
-              <Form.Field>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Field error={Boolean(errors?.email)}>
                 <label>Email</label>
-                <input placeholder="example@gmail.com" />
+                <input
+                  placeholder="example@gmail.com"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => onInputChange('email', e.target.value)}
+                />
+                {errors.email && (
+                  <Label pointing prompt>
+                    {errors.email.message}
+                  </Label>
+                )}
               </Form.Field>
               <Button type="submit" fluid color="twitter">
                 Reset
