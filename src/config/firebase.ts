ff51be6f271a9b77f266c9e723f3a836ca96f2b9
Gapur Kassym <gapur.kassym@gmail.com>
2020-12-07 23:39:25 +0600

    feat(config): create firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export { firebase }
