3e426ef1d388c5c8446ea113b728b916f91c3e61
Gapur Kassym <gapur.kassym@gmail.com>
2020-12-16 22:49:25 +0600

    feat(authenticated-app): redirect to home

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import { useAuth } from 'context/auth-context'
 import { PageErrorFallback, AppHeader, AppFooter } from 'components'
@@ -13,7 +13,6 @@ import { SheshimCreate } from 'screens/sheshim/sheshim-create/sheshim-create'
 import { UserList } from 'screens/users/user-list/user-list'
 import { UserDetails } from 'screens/users/user-details/user-details'
 import { Profile } from 'screens/profile/profile'
-import { NotFound } from 'screens/not-found/not-found'
 
 export default function AuthenticatedApp() {
   const { user } = useAuth()
@@ -28,7 +27,7 @@ export default function AuthenticatedApp() {
         <Route exact path="/users/:userId" component={UserDetails} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
+        <Redirect to="/" />
       </Switch>
       <AppFooter />
     </ErrorBoundary>
