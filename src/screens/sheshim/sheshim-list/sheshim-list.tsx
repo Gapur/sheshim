79730fb953829e1f3a4d8fd22aa3c2ba5b9a4d82
Gapur Kassym <gapur.kassym@gmail.com>
2020-12-22 23:40:56 +0600

    feat(sheshim-list): list loader

--- src/screens/sheshim/sheshim-list/sheshim-list.tsx
+++ src/screens/sheshim/sheshim-list/sheshim-list.tsx
@@ -2,27 +2,45 @@ import React, { useEffect, useState } from 'react'
 import { Header, Button } from 'semantic-ui-react'
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom'
+import Swal from 'sweetalert2'
 
 import { AppLayout } from 'components'
 import { QuestionView } from 'models'
 import { fetchSheshims } from 'services/firebase/sheshim'
 
 import { SheshimListItem } from './components/sheshim-list-item'
+import { SheshimListLoader } from './components/sheshim-list-loader'
 
 const List = styled.div`
   width: 72%;
 
   .segment {
     display: flex;
+
+    .placeholder {
+      width: 100%;
+    }
   }
 `
 
 export function SheshimList() {
-  const history = useHistory()
   const [sheshims, setSheshims] = useState<QuestionView[]>([])
+  const [loading, setLoading] = useState(true)
+  const history = useHistory()
 
   useEffect(() => {
-    fetchSheshims().then(setSheshims)
+    fetchSheshims()
+      .then((data: QuestionView[]) => {
+        setSheshims(data)
+        setLoading(false)
+      })
+      .catch((err) =>
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message,
+        }),
+      )
   }, [])
 
   return (
@@ -34,9 +52,11 @@ export function SheshimList() {
         </Button>
       </Header>
       <List>
-        {sheshims.map((item: QuestionView) => (
-          <SheshimListItem key={item.id} question={item} />
-        ))}
+        {loading ? (
+          <SheshimListLoader />
+        ) : (
+          sheshims.map((item: QuestionView) => <SheshimListItem key={item.id} question={item} />)
+        )}
       </List>
     </AppLayout>
   )
