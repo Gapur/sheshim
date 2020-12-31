30df888708b894723a50b952be307dc14fd6886a
Gapur Kassym <gapur.kassym@gmail.com>
2020-12-31 16:55:33 +0600

    refactor(user-list): update list loader

--- src/screens/users/user-list/components/user-list-loader.tsx
+++ src/screens/users/user-list/components/user-list-loader.tsx
@@ -1,84 +1,38 @@
 import React from 'react'
 import { Table, Placeholder } from 'semantic-ui-react'
 
+const ROW_LIST = Array(4).fill(null)
+
 export function UserListLoader() {
   return (
     <React.Fragment>
-      <Table.Row>
-        <Table.Cell>
-          <Placeholder style={{ height: 36 }}>
-            <Placeholder.Header image>
-              <Placeholder.Line />
-              <Placeholder.Line />
-            </Placeholder.Header>
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Placeholder style={{ height: 36 }}>
-            <Placeholder.Header image>
-              <Placeholder.Line />
-              <Placeholder.Line />
-            </Placeholder.Header>
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Placeholder style={{ height: 36 }}>
-            <Placeholder.Header image>
-              <Placeholder.Line />
-              <Placeholder.Line />
-            </Placeholder.Header>
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-        <Table.Cell>
-          <Placeholder style={{ height: 32 }}>
-            <Placeholder.Image />
-          </Placeholder>
-        </Table.Cell>
-      </Table.Row>
+      {ROW_LIST.map((_, idx: number) => (
+        <Table.Row key={idx}>
+          <Table.Cell>
+            <Placeholder style={{ height: 36 }}>
+              <Placeholder.Header image>
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Header>
+            </Placeholder>
+          </Table.Cell>
+          <Table.Cell>
+            <Placeholder style={{ height: 32 }}>
+              <Placeholder.Image />
+            </Placeholder>
+          </Table.Cell>
+          <Table.Cell>
+            <Placeholder style={{ height: 32 }}>
+              <Placeholder.Image />
+            </Placeholder>
+          </Table.Cell>
+          <Table.Cell>
+            <Placeholder style={{ height: 32 }}>
+              <Placeholder.Image />
+            </Placeholder>
+          </Table.Cell>
+        </Table.Row>
+      ))}
     </React.Fragment>
   )
 }
